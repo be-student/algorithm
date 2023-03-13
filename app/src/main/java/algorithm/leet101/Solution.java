b2d7945eb3a337662aa50cf415321b47bbc79d65
Be-student <presentsong@naver.com>
2023-03-14 00:09:36 +0900

    23/03/13

---
+++ app/src/main/java/algorithm/leet101/Solution.java
@@ -0,0 +1,41 @@
+package algorithm.leet101;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        return recursive(root.left, root.right);
+    }
+
+    private boolean recursive(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return recursive(left.left, right.right) && recursive(left.right, right.left);
+    }
+}
